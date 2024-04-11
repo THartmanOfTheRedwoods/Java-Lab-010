8109f9bd7de39583c31a5862396021f6bc56bb01
vathao777 <vathao777@gmail.com>
2024-04-10 18:45:52 -0700

    Finished Lab 10

---
+++ src/Rational.java
@@ -0,0 +1,96 @@
+public class Rational {
+
+    int numerator, denominator;
+
+    public Rational() {
+        this.numerator = 0;
+        this.denominator = 0;
+    }
+
+    public Rational(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public static void printRational(Rational rational) {
+        System.out.println(rational.numerator + "/" + rational.denominator);
+    }
+
+
+    public static void negate( Rational rational) {
+        int numerator = rational.numerator;
+        int negatedNumerator = -1 * numerator;
+        rational.numerator = negatedNumerator;
+    }
+
+    public static void invert(Rational rational) {
+        int oldNumerator = rational.numerator;
+        int oldDenominator = rational.denominator;
+        rational.numerator = oldDenominator;
+        rational.denominator = oldNumerator;
+    }
+
+    public static double toDouble(Rational rational) {
+        double numerator = rational.numerator;
+        double denominator = rational.denominator;
+        return numerator / denominator;
+    }
+
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        int r = a % b;
+        return gcd(b, r);
+    }
+
+    public static void reduce(Rational rational) {
+        int gcd = gcd(rational.numerator, rational.denominator);
+        rational.numerator = rational.numerator / gcd;
+        rational.denominator = rational.denominator / gcd;
+    }
+
+
+    public static Rational add(Rational rational1, Rational rational2) {
+        int newNumerator = rational1.numerator * rational2.denominator
+                + rational1.denominator * rational2.numerator;
+        int newDenominator = rational1.denominator * rational2.denominator;
+        Rational newRational = new Rational(newNumerator, newDenominator);
+        reduce(newRational);
+        return newRational;
+    }
+
+    public static void main(String[] args) {
+
+        Rational rational1 = new Rational();
+        rational1.numerator = -2;
+        rational1.denominator = 3;
+        printRational(rational1);
+
+
+        Rational rational2 = new Rational(4, 5);
+        printRational(rational2);
+
+
+        negate(rational1);
+        printRational(rational1); // 2/3
+
+
+        invert(rational1);
+        printRational(rational1); // 3/2
+
+
+        double decimal = toDouble(rational1);
+        System.out.println(decimal); // 1.5
+
+
+        Rational rational3 = new Rational(7, 21);
+        printRational(rational3);
+        reduce(rational3);
+        printRational(rational3);
+
+        printRational(add(rational2, rational3));
+    }
+
+}
